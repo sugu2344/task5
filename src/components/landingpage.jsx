a8da9183b04340bfb50b18d024594a8ddb0da71b
sugu2344 <suganesh234@gmail.com>
2024-12-10 12:38:31 +0530

    button alignment fixed

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -19,19 +19,19 @@ const LandingPage = ({ addToCart }) => {
       {post.map((item) => (
         <div
           key={item.id}
-          className="w-full sm:w-[45%] md:w-[30%] lg:w-[23%] border-2 bg-white p-4"
+          className="w-full sm:w-[45%] md:w-[30%] lg:w-[23%] border-2 bg-white p-4 flex flex-col"
         >
           <img
             src={item.image}
             alt={item.title}
             className="w-full h-48 object-cover"
           />
-          <div className="text-center space-y-3 mt-3">
+          <div className="flex flex-col justify-between flex-grow text-center space-y-3 mt-3">
             <h1 className="font-bold text-2xl">{item.title}</h1>
-            <p className="text-lg font-medium">${item.price}</p>
+            <p className="text-lg font-medium">₹{item.price}</p>
             <button
               onClick={() => handleAddToCart(item)}
-              className="border-2 p-2 rounded-xl hover:bg-black hover:text-white cursor-pointer"
+              className="border-2 p-2 rounded-xl hover:bg-black hover:text-white cursor-pointer mt-auto"
             >
               Add to Cart
             </button>
